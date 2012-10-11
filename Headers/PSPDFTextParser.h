cdb018e38d46451846cb6a78dc1e15716364a626
Alex Soto <dxdream@gmail.com>
2012-10-11 09:24:07 -0500

    Compare headers!!

---
+++ Headers/PSPDFTextParser.h
@@ -0,0 +1,34 @@
+//
+//  PSPDFTextParser.h
+//  PSPDFKit
+//
+//  Copyright 2012 Peter Steinberger. All rights reserved.
+//
+
+#import "PSPDFKitGlobal.h"
+
+@class PSPDFFontInfo, PSPDFGraphicsState, PSPDFHighlightAnnotation;
+
+// Enable to log state while parsing.
+extern BOOL kPSPDFTextParserDebugLogEnabled;
+
+/// Parses the text of a PDF page.
+@interface PSPDFTextParser : NSObject
+
+/// Complete page text.
+@property(nonatomic, strong) NSString *text;
+
+/// Complete list of PSPDFGlyph objects. Corresponds to the text.
+@property(nonatomic, strong) NSMutableArray *glyphs;
+
+/// List of detected words (PSPDFWord)
+@property(nonatomic, strong) NSMutableArray *words;
+
+/// List of detected text blocks (PSPDFTextBlock)
+@property(nonatomic, strong, readonly) NSArray *textBlocks;
+
+/// Designated initializer.
+// Page is just used to aid debugging. (absolute to PSPDFDocument)
+- (id)initWithPDFPage:(CGPDFPageRef)pageRef page:(NSUInteger)page;
+
+@end
